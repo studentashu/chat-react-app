27197453001bee61f64612869050eeabf81ddd75
studentashu <ponnuashwini6@gmail.com>
2025-04-16 12:18:03 +0530

    Updated frontend to use Render backend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-backend-app-seyx.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
